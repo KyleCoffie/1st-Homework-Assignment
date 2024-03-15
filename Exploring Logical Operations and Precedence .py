9c8ded7b88419a65fba723a1d83ce78a99f62e37
KyleCoffie <kyle.coffie@live.com>
2024-03-15 08:52:46 -0500

    1st Assignment

---
+++ Exploring Logical Operations and Precedence .py
@@ -0,0 +1,32 @@
+# Task 1: Simple Logic Puzzles
+
+a=9
+b=6
+
+print (a <=13) and (b >=6)
+
+print (a <=13) or (b >=6)
+
+print (not a == b)
+
+# Task 2: Validating Caculations
+
+variable_a = 7*5+9/56
+print (variable_a)
+
+variable_b=7*(5+9)/56
+print(variable_b)
+
+# Task 3:mix and match
+
+a = 3
+b = 2
+
+print (a+b==5)
+
+TA_is_nice= True
+BMW_is_nice=True
+
+Good_car=TA_is_nice and BMW_is_nice
+
+print(Good_car)
---
+++ Understanding Assignmentsand Comparisons.py
@@ -0,0 +1,11 @@
+# Task 1: Value Swapping
+x = 657+896*(5+3)
+print (x)
+
+y = 657*896+(5+3)
+print (y)
+
+# Task 2 Perfect Square Checker
+input = 9
+result= input**.5
+print (result==int(result))
